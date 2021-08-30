5a7cbe0ce94152518688c1312d140f26ba055cc9
Артур <arturskomarohov@gmail.com>
2021-08-30 18:25:56 +0300

    Добавьте файлы проекта.

---
+++ SampleForThreading/FigureCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SampleForThreading
+{
+    class FigureCalculator
+    {
+        private object _locker;
+        public List<Figure> figures = new List<Figure>();
+        public FigureCalculator(object locker)
+        {
+            _locker = locker;
+        }
+        public void PrintFigures()
+        {
+            ThreadPool.QueueUserWorkItem(_ => 
+            {
+                while (true)
+                {
+                    lock (_locker)
+                    {
+                        foreach (var item in figures)
+                        {
+                            Console.WriteLine($"{item.SideCount}");
+                        }
+                    }
+                    Thread.Sleep(500);
+                }
+            });
+        }
+    }
+}
---
+++ SampleForThreading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace SampleForThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            var t = new Thread(PrintX);
+            t.Start();
+            while (true)
+            {
+                Console.WriteLine("Y");
+            }
+        }
+
+        private static void PrintX()
+        {
+            while (true)
+            {
+                Console.WriteLine("X");
+            }
+        }*/
+            var locker = new object();
+            var figureCalc = new FigureCalculator(locker);
+            figureCalc.PrintFigures();
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    lock (locker)
+                    {
+                        figureCalc.figures.Add(new Figure { SideCount = i });
+                    }
+                    Thread.Sleep(2000);
+                }
+            });
+    }
+}
