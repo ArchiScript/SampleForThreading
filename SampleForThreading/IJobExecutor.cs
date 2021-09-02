0d7541ccce547b83aa965711fc3ca466625c4eb2
Артур <arturskomarohov@gmail.com>
2021-09-02 22:06:21 +0300

    d

---
+++ SampleForThreading/IJobExecutor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleForThreading
+{
+   public interface IJobExecutor
+    {
+
+        int Amount { get; }
+        void Start(int maxConcurrent);
+        void Stop();
+        void Add(Action action);
+        void Clear();
+    }
+}
---
+++ SampleForThreading/JobExecutor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleForThreading
+{
+    class JobExecutor : IJobExecutor
+    {
+        public int Amount { get; }
+        public void Start(int maxConcurrent)
+        {
+            Task[] taskArray = new Task[maxConcurrent];
+            foreach (var task in taskArray)
+            {
+                task.Start();
+            }
+        }
+        public void Stop()
+        {
+        }
+        public void Add(Action action)
+        {
+        }
+        public void Clear()
+        {
+        }
+    }
+}
