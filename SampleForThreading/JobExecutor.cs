45c5fad4e5687de26518e13a0c220e4210c9c121
Артур <arturskomarohov@gmail.com>
2021-09-03 12:31:53 +0300

    try IJobExecutor

--- SampleForThreading/JobExecutor.cs
+++ SampleForThreading/JobExecutor.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace SampleForThreading
 {
@@ -10,11 +12,26 @@ namespace SampleForThreading
         public int Amount { get; }
         public void Start(int maxConcurrent)
         {
-            Task[] taskArray = new Task[maxConcurrent];
+            /*Task[] taskArray = new Task[maxConcurrent];
             foreach (var task in taskArray)
             {
                 task.Start();
             }
+            ConcurrentQueue<Task> tasks = new ConcurrentQueue<Task>();
+            foreach (var t in tasks)
+            {
+                t.Start();
+            }*/
+            Semaphore semaphore = new Semaphore(0, maxConcurrent);
+
+            for (int i = 1; i < Amount; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(???));
+                t.Start(i);
+            }
+
+
+
         }
         public void Stop()
         {
@@ -24,6 +41,7 @@ namespace SampleForThreading
         }
         public void Clear()
         {
+           
         }
     }
 }
